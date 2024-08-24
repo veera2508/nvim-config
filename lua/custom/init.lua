b41bb90445ae32feb7ffda2371191a62b1e427d2
Veeraraghavan Narasimhan <virunarasimhan25@gmail.com>
2024-08-24 00:08:02 -0400

    changes

--- lua/custom/init.lua
+++
@@ -1,7 +0,0 @@
-vim.opt.relativenumber = true
-vim.opt.relativenumber = true
-vim.opt.splitright = true
-vim.opt.wrap = false
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.clipboard = "unnamedplus"
