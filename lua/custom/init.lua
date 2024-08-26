a6b3ad079df1eb3e2910e92586eedfa3a2a2160d
Veeraraghavan Narasimhan <virunarasimhan25@gmail.com>
2024-08-26 11:55:57 -0400

    Revert "changes"
    This reverts commit b41bb90445ae32feb7ffda2371191a62b1e427d2.

---
+++ lua/custom/init.lua
@@ -0,0 +1,7 @@
+vim.opt.relativenumber = true
+vim.opt.relativenumber = true
+vim.opt.splitright = true
+vim.opt.wrap = false
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.clipboard = "unnamedplus"
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,27 @@
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+        "jdtls",
+        "pyright",
+        "clangd"
+      }
+    }
+  },
+  {
+    "yutkat/confirm-quit.nvim",
+    event = "CmdlineEnter",
+    opts = {},
+  },
+}
+
+return plugins
